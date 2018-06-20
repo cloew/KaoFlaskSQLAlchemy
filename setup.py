ed86e1024e6fd87aecd3fa3a11f49f944edb28ad
Chris Loew <cloew123@gmail.com>
2018-06-19 18:59:07 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='kao_flask_sqlalchemy',
-      version='0.1.1',
+      version='0.1.2',
       description="",
       author='',
       author_email='',
