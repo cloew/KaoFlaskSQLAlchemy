7347265f8b888d03b7899e7b020da3ba0d0424d5
Chris Loew <cloew123@gmail.com>
2021-08-09 12:12:34 -0600

    Tidied up some small issues in the CrudEndpoints class

--- kao_flask_sqlalchemy/crud/crud_endpoints.py
+++ kao_flask_sqlalchemy/crud/crud_endpoints.py
@@ -10,7 +10,7 @@ def apply_decorators(fn, decorators):
     return fn
 
 class CrudEndpoints:
-    """ Represents the standard CRUD Endpoints for a particualr model class """
+    """ Represents the standard CRUD Endpoints for a particular model class """
     
     def __init__(self, rootUrl, modelCls, toJson, routeParams={}, jsonColumnMap={}, decorators=[]):
         """ Initialize with the root URL and the model class to wrap """
@@ -21,11 +21,6 @@ class CrudEndpoints:
         self.decorators = decorators
         
         self.recordValueProvider = RecordValueProvider(jsonColumnMap)
-        # self.listEndpoint = Endpoint(rootUrl, get=ListController(modelCls, toJson, routeParams=routeParams, decorators=decorators), 
-                                              # post=CreateController(modelCls, toJson, routeParams=routeParams, recordValueProvider=self.recordValueProvider, decorators=decorators))
-        # self.recordEndpoint = Endpoint(rootUrl+'/<int:id>', get=RecordController(modelCls, toJson, decorators=decorators), 
-                                                            # put=UpdateController(modelCls, toJson, recordValueProvider=self.recordValueProvider, decorators=decorators), 
-                                                            # delete=DeleteController(modelCls, decorators=decorators))
 
     @with_json
     def list(self, *, json, **kwargs):
@@ -84,7 +79,7 @@ class CrudEndpoints:
     def record_endpoint(self):
         """ Return the endpoints """
         return Endpoint(self.rootUrl+'/<int:id>', get=self.with_decorators(self.read), put=self.with_decorators(self.update), delete=self.with_decorators(self.delete))
-        
+    
     def with_decorators(self, fn):
       """ Return the fn with the decorators the class was instantiated with """
       return apply_decorators(fn, self.decorators)
